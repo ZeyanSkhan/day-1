6026fe93836735fad19134bcb340590f38eaa3e7
ZeyanSkhan <zeyansk2004@gmail.com>
2026-02-15 00:14:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function ssText() {
-  let nText = "Text changed using JavaScript!";
-  document.getElementById("youknow").innerText = nText;
+let A = true;
+
+function toggle() {
+  if (A) {
+    document.getElementById("message").innerText = "Yo how are u?";
+    A = false;
+  } else {
+    document.getElementById("message").innerText = "Happened isn't it";
+    A = true;
+  }
 }
